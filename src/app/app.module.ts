d8502f4b3393abfaae9f6ae47e9c3836b8103731
subhashiscse <smollickcseiu@gmail.com>
2025-07-01 17:14:46 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
